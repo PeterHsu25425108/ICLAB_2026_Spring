e28c5444ad698866510f8b3193c0d4c943e53820
PeterHsu25425108 <25425108a@gmail.com>
2026-03-04 15:07:08 +0800

    Lab1 Init

---
+++ SPRING2026_LAB01/01_RTL/DRCA.v
@@ -0,0 +1,37 @@
+module DRCA (
+    input [3:0]  drc_sel,
+    input [18:0] shape0 ,
+    input [18:0] shape1 ,
+    input [18:0] shape2 ,
+    input [18:0] shape3 ,
+    input [18:0] shape4 ,
+    input [18:0] shape5 ,
+    input [18:0] shape6 ,
+    input [18:0] shape7 ,
+    input [18:0] shape8 ,
+    input [18:0] shape9 ,
+    input [18:0] shape10 ,
+    input [18:0] shape11 ,
+    input [18:0] shape12 ,
+    input [18:0] shape13 ,
+    input [18:0] shape14 ,
+    input [18:0] shape15 ,
+    output [4:0] drc_out
+);
+
+
+//**************************************************
+// Parameter 
+//**************************************************
+
+
+//**************************************************
+// Reg & Wire 
+//**************************************************
+
+
+//**************************************************
+// Design 
+//**************************************************
+
+endmodule
