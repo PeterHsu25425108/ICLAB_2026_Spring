2185c05ca6eedd498ab6c0b42f7e8802f92b3eb0
PeterHsu25425108 <25425108a@gmail.com>
2026-04-05 13:59:28 +0800

    Prev ver drives sram din from primary input, fixed this by delaying weight sram command

--- SPRING2026_LAB05/01_RTL/DM.v
+++ SPRING2026_LAB05/01_RTL/DM.v
@@ -626,33 +626,35 @@ end
 
 // SRAM Command & Write Data Generation
 // Command, Din, and Address updates
-always @(*) begin : weight_sram_cmd_and_din_ctrl
-    if(weight_sram_write_cnt == write_threshold && 
-        (state == LOAD_DS_CONV_WEIGHT || state == LOAD_US_CONV_WEIGHT || state == LOAD_PROJ_WEIGHT))begin
-        weight_sram_cmd = `WRITE;
-        // Format data based on the current state
-        if (state == LOAD_PROJ_WEIGHT) begin
-            // 128-bit full payload
-            din_weight = {weight_sram_write_buf[123:0], i_weight}; //weight_sram_write_buf[127:0];
-        end else begin
-            // 72-bit payload (17 previous elements + current element), padded with 56 zeros
-            din_weight = (i_valid) ? {56'd0, weight_sram_write_buf[67:0], i_weight} : {56'd0, weight_sram_write_buf[71:0]}; //{56'd0, weight_sram_write_buf[71:0]};
-        end
-    end else begin
-        weight_sram_cmd = `STANDBY;
-        din_weight = 0;
-    end
+always @(*) begin : weight_sram__din_ctrl
+    din_weight = weight_sram_write_buf;
+    // if(weight_sram_write_cnt == write_threshold && 
+    //     (state == LOAD_DS_CONV_WEIGHT || state == LOAD_US_CONV_WEIGHT || state == LOAD_PROJ_WEIGHT))begin
+    //     weight_sram_cmd = `WRITE;
+    //     // Format data based on the current state
+    //     if (state == LOAD_PROJ_WEIGHT) begin
+    //         // 128-bit full payload
+    //         din_weight = {weight_sram_write_buf[123:0], i_weight}; //weight_sram_write_buf[127:0];
+    //     end else begin
+    //         // 72-bit payload (17 previous elements + current element), padded with 56 zeros
+    //         din_weight = (i_valid) ? {56'd0, weight_sram_write_buf[67:0], i_weight} : {56'd0, weight_sram_write_buf[71:0]}; //{56'd0, weight_sram_write_buf[71:0]};
+    //     end
+    // end else begin
+    //     weight_sram_cmd = `STANDBY;
+    //     din_weight = 0;
+    // end
 end
 
 always @(posedge clk or negedge rst_n) begin : weight_wr_addr_ctrl
     if (!rst_n) begin
         weight_wr_addr <= 0;
-
+        weight_sram_cmd <= `STANDBY;
     end else if (weight_sram_write_cnt == write_threshold 
         && (state == LOAD_DS_CONV_WEIGHT || state == LOAD_US_CONV_WEIGHT || state == LOAD_PROJ_WEIGHT)) begin
         weight_wr_addr <= weight_wr_addr + 1;
+        weight_sram_cmd <= `WRITE;
     end else begin // TODO: we need to reset weight_wr_addr for future reads
-        // weight_sram_cmd <= `STANDBY;
+        weight_sram_cmd <= `STANDBY;
         weight_wr_addr <= weight_wr_addr;
     end
 end
