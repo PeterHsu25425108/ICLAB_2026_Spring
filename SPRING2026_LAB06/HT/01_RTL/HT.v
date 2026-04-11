3aabde9aeb524ca100d4e604c9b5e7185e355628
PeterHsu25425108 <25425108a@gmail.com>
2026-04-11 23:34:54 +0800

    area=23186.016072, clk=20 after reducing nxt_merge_nodes

--- SPRING2026_LAB06/HT/01_RTL/HT.v
+++ SPRING2026_LAB06/HT/01_RTL/HT.v
@@ -99,7 +99,7 @@ wire [3:0] curr_sorted_nodes [0:7];
 
 wire [4:0] w6, w7;
 reg [2:0] keep_cnt;
-
+wire [1:0] drop_cnt [0:7];
 // Shared compare signals against current merge targets from sorter
 wire [4:0] root_eq6, root_eq7, root_hit67;
 wire [7:0] merge_eq6, merge_eq7, merge_hit67, merge_keep67, merge_neq6, merge_neq7;
@@ -226,6 +226,15 @@ assign w7 = ({5{merge_eq7[0]}} & merge_weights[0]) |
      ({5{merge_eq7[6]}} & merge_weights[6]) |
      ({5{merge_eq7[7]}} & merge_weights[7]);
 
+assign drop_cnt[0] = 2'd0;
+assign drop_cnt[1] = {1'b0, merge_hit67[0]};
+assign drop_cnt[2] = drop_cnt[1] + {1'b0, merge_hit67[1]};
+assign drop_cnt[3] = drop_cnt[2] + {1'b0, merge_hit67[2]};
+assign drop_cnt[4] = drop_cnt[3] + {1'b0, merge_hit67[3]};
+assign drop_cnt[5] = drop_cnt[4] + {1'b0, merge_hit67[4]};
+assign drop_cnt[6] = drop_cnt[5] + {1'b0, merge_hit67[5]};
+assign drop_cnt[7] = drop_cnt[6] + {1'b0, merge_hit67[6]};
+
 always @(*) begin : nxt_merge_weights_logic
     // Default: hold current values
     for(i=0; i<8; i=i+1) begin
@@ -251,12 +260,34 @@ always @(*) begin : nxt_merge_weights_logic
 
         // 2. Compress the array: shift surviving nodes to the front
         // This preserves their relative priority for the stable sort
-        keep_cnt = 0;
+        // keep_cnt = 0;
+        // for(i=0; i<8; i=i+1) begin
+        //     if (merge_keep67[i]) begin
+        //         nxt_merge_nodes[keep_cnt]   = merge_nodes[i];
+        //         nxt_merge_weights[keep_cnt] = merge_weights[i];
+        //         keep_cnt = keep_cnt + 1;
+        //     end
+        // end
         for(i=0; i<8; i=i+1) begin
-            if (merge_keep67[i]) begin
-                nxt_merge_nodes[keep_cnt]   = merge_nodes[i];
-                nxt_merge_weights[keep_cnt] = merge_weights[i];
-                keep_cnt = keep_cnt + 1;
+            nxt_merge_nodes[i]   = merge_nodes[i];
+            nxt_merge_weights[i] = merge_weights[i];
+        end 
+
+        for(i=0; i<6; i=i+1) begin
+            // stay at the same position
+            if (merge_keep67[i] && drop_cnt[i] == 2'd0) begin
+                nxt_merge_nodes[i]   = merge_nodes[i];
+                nxt_merge_weights[i] = merge_weights[i];
+            end
+            // shift 1 position from the right
+            else if (merge_keep67[i+1] && drop_cnt[i+1] == 2'd1) begin
+                nxt_merge_nodes[i]   = merge_nodes[i+1];
+                nxt_merge_weights[i] = merge_weights[i+1];
+            end
+            // shift 2 positions from the right
+            else begin
+                nxt_merge_nodes[i]   = merge_nodes[i+2];
+                nxt_merge_weights[i] = merge_weights[i+2];
             end
         end
 
@@ -305,7 +336,7 @@ SORT_IP #(.IP_WIDTH(8)) sorter(
                 merge_weights[4], merge_weights[5], merge_weights[6], merge_weights[7]})
 );
  
- 
+
 // combinationally unpack sorter output (order preserved)
 generate
     for(idx=0;idx<8;idx=idx+1)begin
